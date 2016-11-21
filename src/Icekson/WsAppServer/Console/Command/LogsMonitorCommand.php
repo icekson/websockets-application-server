305821db0f7dbccba735ade896c9fd6f18a7c63d
Alexey Itsekson <a.itsekson@program-ace.com.ua>
2016-11-21 15:19:24 +0200

    added monitor logger, fixed class extending

--- src/Icekson/WsAppServer/Console/Command/LogsMonitorCommand.php
+++ src/Icekson/WsAppServer/Console/Command/LogsMonitorCommand.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\StreamOutput;
 use Utils\ConsoleTextColorizator;
 
-class LogsMonitorCommand
+class LogsMonitorCommand extends BaseCommand
 {
     protected function configure()
     {
